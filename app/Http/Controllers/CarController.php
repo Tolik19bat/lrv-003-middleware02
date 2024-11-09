6ef801226025c124ca91bf589f4cc5d8279d3f61
Anatoly Batov <play.hoffa@gmail.com>
2024-11-09 20:35:51 +0400

    first commit

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CarController extends Controller
+{
+    
+    public function index() {
+        return view('car.index', [
+            'company' => 'моя защищённая страница',
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/cars', [\App\Http\Controllers\CarController::class, 'index']);
+});
+
