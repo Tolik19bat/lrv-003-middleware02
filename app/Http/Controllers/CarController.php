ba9685f64224ff60b4d0ea028b299b2f52e19fd7
Anatoly Batov <play.hoffa@gmail.com>
2024-11-10 04:32:20 +0400

    обратная правка текста

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -10,7 +10,7 @@ class CarController extends Controller
     
     public function index() {
         return view('car.index', [
-            'company' => 'моя защещённая страница',
+            'company' => 'моя защищённая страница',
         ]);
     }
 }
